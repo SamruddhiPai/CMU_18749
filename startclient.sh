86c17d01f5a27994d1b1852dd40eff0aa863470d
SamruddhiPai <samruddhipai98@gmail.com>
2021-09-19 11:23:24 -0400

    syncing with main

--- startclient.sh
+++
@@ -1,7 +0,0 @@
-
-osascript -e 'tell app "Terminal"
-    do script "cd /Users/riturajsingh/BigFolder/courses/2_18749_Reliable_DS/CMU_18749 && python3 client1.py"
-    do script "cd /Users/riturajsingh/BigFolder/courses/2_18749_Reliable_DS/CMU_18749 && python3 client2.py"
-    do script "cd /Users/riturajsingh/BigFolder/courses/2_18749_Reliable_DS/CMU_18749 && python3 client3.py"
-    do script "cd /Users/riturajsingh/BigFolder/courses/2_18749_Reliable_DS/CMU_18749 && python3 status.py"
-end tell'
