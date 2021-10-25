af60b0f9db6a329f35ed3b6a579120c99c29fbef
Ravi-ai1994 <66255244+Ravi-ai1994@users.noreply.github.com>
2021-10-24 22:32:47 -0400

    Updating status of individual clients

--- GFD.py
+++ GFD.py
@@ -25,34 +25,52 @@ def service_connection(key, mask):
         recv_data = sock.recv(1024)  # Should be ready to read
         recv_data_str = str(recv_data)
         datalist = recv_data_str.split(";")
-        if len(datalist) > 2:
-            recv_data_str = str(recv_data)
-            datalist = recv_data_str.split(";")
-            add_members = datalist[2]
-            if "S1" in add_members and "add" in add_members:
-                membership.add("S1")
-            elif "S1" in add_members and "delete" in add_members:
-                membership.discard("S1")
-            if "S2" in add_members and "add" in add_members:
-                membership.add("S2")
-            elif "S2" in add_members and "delete" in add_members:
-                membership.discard("S2")
-            if "S3" in add_members and "add" in add_members:
-                membership.add("S3")
-            elif "S3" in add_members and "delete" in add_members:
-                membership.discard("S3")
-            if len(membership) == 0:
-                update = "High Alert! All server replicas dead!"
-            else:
-                update = "GFD: " + str(len(membership)) + " members:" + str(membership)
-            log(update)
-            print("---------------------------------")
-            data.outb = b'Acknowledgement'
-            #print('Updated data.outb')
-                
-            # except:
-            #     if (str(recv_data_str) == "b'Are you alive?'"):
-            #         data.outb = b'I am alive!'
+        if recv_data:
+            print(recv_data)
+            if recv_data == b'Send me the Status':
+                s = "+"
+                if "S1" in membership:
+                    s += "S1"
+                if "S2" in membership:
+                    s += "S2"
+                if "S3" in membership:
+                    s += "S3"
+                data.outb = bytes(s, 'utf-8')
+            print(data.outb)
+            if len(datalist) > 2:
+                recv_data_str = str(recv_data)
+                datalist = recv_data_str.split(";")
+                add_members = datalist[2]
+                if "S1" in add_members and "add" in add_members:
+                    membership.add("S1")
+                    data.outb = b"Add S1"
+                elif "S1" in add_members and "delete" in add_members:
+                    membership.discard("S1")
+                    data.outb = b"Remove S1"
+                if "S2" in add_members and "add" in add_members:
+                    membership.add("S2")
+                    data.outb = b"Add S2"
+                elif "S2" in add_members and "delete" in add_members:
+                    membership.discard("S2")
+                    data.outb = b"Remove S2"
+                if "S3" in add_members and "add" in add_members:
+                    membership.add("S3")
+                    data.outb = b"Add S3"
+                elif "S3" in add_members and "delete" in add_members:
+                    membership.discard("S3")
+                    data.outb = b"Remove S3"
+                if len(membership) == 0:
+                    update = "High Alert! All server replicas dead!"
+                else:
+                    update = "GFD: " + str(len(membership)) + " members:" + str(membership)
+                log(update)
+                print("---------------------------------")
+                # data.outb = b'Acknowledgement'
+                #print('Updated data.outb')
+                    
+                # except:
+                #     if (str(recv_data_str) == "b'Are you alive?'"):
+                #         data.outb = b'I am alive!'
         else:
             log(("Closing connection to " + str(data.addr)))
             sel.unregister(sock)
@@ -62,7 +80,7 @@ def service_connection(key, mask):
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:] #to clear data.outb
 
-host, port = '127.0.0.1', 1234
+host, port = '127.0.0.1', 1285
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
