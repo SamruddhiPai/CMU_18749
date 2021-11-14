ca18eee649374d3a9fe021959d0d32fa06508c7f
Manasi Purohit <manasipurohit8@gmail.com>
2021-11-13 23:15:31 -0500

    passive server 1 changes

--- Passive_Replication/passive_server_1.py
+++ Passive_Replication/passive_server_1.py
@@ -161,6 +161,86 @@ class Server_as_Client(Thread):
         except KeyboardInterrupt:
             print("caught keyboard interrupt, exiting")
 
+'''
+class Server_as_Primary_Replica(Thread):
+    #def __init__(self, host, port1, port2, sel1, sel2):
+    def __init__(self, host, port1, sel1):
+        Thread.__init__(self)
+        self.host = host
+        self.port1 = port1
+        self.sel1 = sel1
+        # self.port2 = port2
+        # self.sel2 = sel2
+
+    def accept_wrapper(self, sock, sel):
+        conn, addr = sock.accept()  # Should be ready to read
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        sel.register(conn, events, data=data)
+
+    def service_connection(self, key, mask, sel):
+        print('Service Connection!!!!!!!!!!')
+        global X
+        global CHECK_POIN_NUM
+        sock = key.fileobj
+        data = key.data
+                    
+        if mask & selectors.EVENT_WRITE:
+            state = X
+            checkpoint_msg = str(state) + ';' + str(CHECK_POIN_NUM)
+            data.outb = bytes(checkpoint_msg, 'utf-8')
+            try:
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:] #to clear data.outb
+            except:
+                log(("Closing connection to " + str(data.addr)))
+                sel.unregister(sock)
+                sock.close()
+
+        
+    def run(self):
+        global CHECK_POIN_NUM
+        lsock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lsock1.bind((self.host, self.port1))
+        lsock1.listen()
+        print("listening on", (self.host, self.port1))
+        lsock1.setblocking(False)
+        self.sel1.register(lsock1, selectors.EVENT_WRITE | selectors.EVENT_READ , data=None)
+
+        # lsock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # lsock2.bind((self.host, self.port2))
+        # lsock2.listen()
+        # print("listening on", (self.host, self.port2))
+        # lsock2.setblocking(False)
+        # self.sel2.register(lsock2, selectors.EVENT_WRITE | selectors.EVENT_READ, data=None)
+
+        try:
+            while True:
+                events1 = self.sel1.select(timeout=None) # Blocks until client ready for I/O, in effect till client sends data
+                for key, mask in events1:
+                    if key.data is None: # key.data opaque class, will be assigned to ceratin type by client(ex: types.SimpleNamespace)
+                        self.accept_wrapper(key.fileobj, self.sel1)
+                    else:
+                        self.service_connection(key, mask, self.sel1)
+                        print("Checkpoint Sent to Backup Replica Server 2")
+
+                # events2 = self.sel2.select(timeout=None)
+                # for key, mask in events2:
+                #     if key.data is None: # key.data opaque class, will be assigned to ceratin type by client(ex: types.SimpleNamespace)
+                #         self.accept_wrapper(key.fileobj, self.sel2)
+                #     else:
+                #         self.service_connection(key, mask, self.sel2)
+                #         print("Checkpoint Sent to Backup Replica Server 3")
+                CHECK_POIN_NUM += 1
+                time.sleep(CHECK_POINT_FREQ)
+
+        except KeyboardInterrupt:
+            print("caught keyboard interrupt, exiting")
+        finally:
+            self.sel1.close()
+            # self.sel2.close()
+'''
 
 class Server_as_Primary_Replica(Thread):
     def __init__(self, host, port1, port2, sel1, sel2):
@@ -216,7 +296,7 @@ class Server_as_Primary_Replica(Thread):
 
         try:
             while True:
-                events1 = self.sel1.select(timeout=None) # Blocks until client ready for I/O, in effect till client sends data
+                events1 = self.sel1.select(timeout=0.5) # Blocks until client ready for I/O, in effect till client sends data
                 for key, mask in events1:
                     if key.data is None: # key.data opaque class, will be assigned to ceratin type by client(ex: types.SimpleNamespace)
                         self.accept_wrapper(key.fileobj, self.sel1)
@@ -224,7 +304,7 @@ class Server_as_Primary_Replica(Thread):
                         self.service_connection(key, mask, self.sel1)
                         print("Checkpoint Sent to Backup Replica Server 2")
 
-                events2 = self.sel2.select(timeout=None)
+                events2 = self.sel2.select(timeout=0.5)
                 for key, mask in events2:
                     if key.data is None: # key.data opaque class, will be assigned to ceratin type by client(ex: types.SimpleNamespace)
                         self.accept_wrapper(key.fileobj, self.sel2)
@@ -239,7 +319,6 @@ class Server_as_Primary_Replica(Thread):
         finally:
             self.sel1.close()
             self.sel2.close()
-
 connid = 1
 
 # host_s, port_s = '127.0.0.1', 1234
@@ -259,10 +338,15 @@ server_as_client.start()
 
 # Establishing Connection to replica S2 and S3
 host_s, port_s2, port_s3 = config.server_1_ip, config.server_1_listen_s2, config.server_1_listen_s3
-sel_server1 = selectors.DefaultSelector()
 sel_server2 = selectors.DefaultSelector()
-server_as_primary_replica1 = Server_as_Primary_Replica(host_s, port_s2, port_s3, sel_server1, sel_server2)
+sel_server3 = selectors.DefaultSelector()
+
+server_as_primary_replica1 = Server_as_Primary_Replica(host_s, port_s2, port_s3, sel_server2, sel_server3)
 server_as_primary_replica1.start()
 
+#server_as_primary_replica1 = Server_as_Primary_Replica(host_s, port_s2, sel_server2)
+#server_as_primary_replica1.start()
+#server_as_primary_replica2 = Server_as_Primary_Replica(host_s, port_s3, sel_server3)
+#server_as_primary_replica2.start()
 
 
