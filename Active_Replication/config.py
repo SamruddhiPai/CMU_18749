9d68a975d3e469a3ed5faebaf2602f15f2887862
Manasi Purohit <manasipurohit8@gmail.com>
2021-12-07 04:19:04 -0500

    Adding config file

--- Active_Replication/config.py
+++ Active_Replication/config.py
@@ -12,7 +12,7 @@ gfd_sendto = rm_listen
 ############################################
 
 #server 1
-server_1_ip = "10.0.0.123"
+server_1_ip = "10.0.0.163"
 server_1_listen = 1200
 server_1_sendto = 4000
 
@@ -20,29 +20,37 @@ server_1_listen_s2 = 1300
 server_1_listen_s3 = 1301
 
 #LFD 1
-lfd_1_ip = "10.0.0.123"
+lfd_1_ip = "10.0.0.163"
 lfd_1_listen = server_1_sendto
 lfd_1_sendto = gfd_listen
 
 ############################################
 
 #server 2
-server_2_ip = "10.0.0.123"
+server_2_ip = "10.0.0.163"
 server_2_listen = 1201
 server_2_sendto = 4001
+
+server_2_listen_s1 = 1302
+server_2_listen_s3 = 1303
+
 #LFD 2
-lfd_2_ip = "10.0.0.123"
+lfd_2_ip = "10.0.0.163"
 lfd_2_listen = server_2_sendto
 lfd_2_sendto = gfd_listen
 
 ############################################
 
 #server 3
-server_3_ip = "10.0.0.123"
+server_3_ip = "10.0.0.163"
 server_3_listen = 1202
 server_3_sendto = 4002
+
+server_3_listen_s1 = 1304
+server_3_listen_s2 = 1305
+
 #LFD 3
-lfd_3_ip = "10.0.0.123"
+lfd_3_ip = "10.0.0.163"
 lfd_3_listen = server_3_sendto
 lfd_3_sendto = gfd_listen
 
