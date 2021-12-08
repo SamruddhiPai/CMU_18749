54790d9c0f807d6bb3987d809d8009624e3a3085
Rituraj Singh <riturajsingh@riturajs-mbp.myfiosgateway.com>
2021-12-08 10:47:16 -0500

    not working

--- Passive_Replication/passive_server_1.py
+++ Passive_Replication/passive_server_1.py
@@ -9,6 +9,7 @@ import time
 from util import log
 from threading import Thread
 import config
+import primary_status
 
 CHECK_POINT_FREQ = 5 #sends a checkpoint every 10 seconds
 CHECK_POIN_NUM = 0
@@ -159,6 +160,73 @@ class Server_as_Client(Thread):
         except KeyboardInterrupt:
             print("caught keyboard interrupt, exiting")
 
+class Server_as_Client_to_Primary(Thread):
+    def __init__(self, host, port, sel):
+        Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.sel = sel
+
+    def start_connections(self,host, port):
+        server_addr = (host, port)
+        print("Starting connection", connid, "to", server_addr)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        # events = selectors.EVENT_WRITE
+        self.sel.register(sock, events, data=None)
+    
+    def service_connection(self,key, mask, data):
+        sock = key.fileobj
+        #data = key.data
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            print(str(recv_data))
+            if recv_data:
+                recv_data_str = recv_data.decode("utf-8")
+                print(recv_data_str)
+                datalist = recv_data_str.split(';')
+                log('Current State: '+ str(datalist[0]))
+                log('Checkpoint Value: ' + str(datalist[1]))
+                receive_str = "Received status (X) from from Primary Server as " + datalist[0] + " and check point number is "+ datalist[1]
+                log(receive_str)
+                X = int(datalist[0])
+                checkpoint_counter = int(datalist[1])
+                # checkpoint_msg = "Checkpoint counter on server 2 is updated to " + str(checkpoint_counter)
+                # print('-----Received X-----')
+                # log(str(X))
+                # print('-----Received checkpoint is------')
+                # log(str(checkpoint_msg))
+
+                data.recv_total += len(recv_data)
+            if not recv_data or data.recv_total == data.msg_total:
+                close_message = "Closing Connection " + str(data.connid)
+                log(close_message)
+                self.sel.unregister(sock)
+
+    def run(self):
+        self.start_connections(self.host, int(self.port))
+        try:
+            while True:
+                data = types.SimpleNamespace(
+                    connid=CONN_ID,
+                    msg_total=1024,
+                    recv_total=0,
+                    outb=b"",
+                )
+                events = self.sel.select(timeout=1)
+                if events:
+                    for key, mask in events:
+                        self.service_connection(key, mask, data)
+
+        except IOError as e:
+            close_message = "Server got disconnected"
+            log(close_message)
+                
+        except KeyboardInterrupt:
+            print("caught keyboard interrupt, exiting")
+
 class Server_as_Primary_Replica(Thread):
     def __init__(self, host, port1, port2, sel1, sel2):
         Thread.__init__(self)
@@ -251,6 +319,21 @@ host_c, port_c = config.server_1_ip, config.server_1_sendto
 sel_client = selectors.DefaultSelector()
 server_as_client = Server_as_Client(host_c, port_c, sel_client)
 server_as_client.start()
+
+
+CONN_ID_p = 11
+host_p = None
+port_p = None
+if (primary_status.primary_replica == "S2"):
+    host_p, port_p = config.server_2_ip, config.server_2_listen_s1
+elif (primary_status.primary_replica == "S3"):
+    host_p, port_p = config.server_3_ip, config.server_3_listen_s1
+sel_client_to_p = selectors.DefaultSelector()
+
+
+#needs to be started
+# server_as_client_to_p = Server_as_Client_to_Primary(host_p, port_p, sel_client_to_p)
+# server_as_client_to_p.start()
 
 # Establishing Connection to replica S2 and S3
 host_s, port_s2, port_s3 = config.server_1_ip, config.server_1_listen_s2, config.server_1_listen_s3
